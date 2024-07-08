8af7b73afbd0ed51228c3d0fd08e40f712aa314a
Administrator <wltngus200@gmail.com>
2024-07-08 21:13:21 +0900

    0708

--- src/main/java/com/green/practice_security_social/security/jwt/JwtAuthenticationAccessDeniedHandler.java
+++ src/main/java/com/green/practice_security_social/security/jwt/JwtAuthenticationAccessDeniedHandler.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 import java.io.IOException;
